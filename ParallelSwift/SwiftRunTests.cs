05227fc1691625145537b2594194db4e123f9f4f
J-M <jean-michel.perraud@csiro.au>
2015-07-03 17:00:51 +1000

    Define further goals of runtime performance measurement. Start an Rmd file.

---
+++ ParallelSwift/SwiftRunTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swift.Bindings;
+
+namespace ParallelSwift
+{
+    public class SwiftRunTests
+    {
+        public static void ParallelRun(params IModelSimulation[] simulations)
+        {
+            var parallelOptions = new ParallelOptions() { MaxDegreeOfParallelism = -1 };
+            Parallel.ForEach(simulations, parallelOptions,
+                s => s.Execute()
+            );
+        }
+
+        public static double[] ParallelRun(IModelSimulation simulation, int numSystems, int reps)
+        {
+            var systems = new List<IModelSimulation>();
+            for (int i = 0; i < numSystems; i++)
+            {
+                systems.Add(simulation.CloneModel());
+            }
+            var parallelOptions = new ParallelOptions() { MaxDegreeOfParallelism = -1 };
+
+            double[] res = new double[reps];
+            var sw = new Stopwatch();
+            for (int rep = 0; rep < reps; rep++)
+            {
+                sw.Start();
+                Parallel.ForEach(systems, parallelOptions, s => s.Execute());
+                sw.Stop();
+                res[rep] = sw.Elapsed.TotalMilliseconds;
+                sw.Reset();
+            }
+            return res;
+        }
+    }
+}
